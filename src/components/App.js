1eb9ce552d5043453adf4b994a6511530704ccca
Loty Gero <lotgyero@gmail.com>
2018-07-10 12:50:00 +0300

    Some clean code

--- src/components/App.js
+++ src/components/App.js
@@ -1,32 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../logo.svg';
+/* import logo from '../logo.svg'; */
 import '../App.css';
-
-
-// import PhotoButton from './Button/Photo';
-// import VideoButton from './Button/Video';
-// <PhotoButton />
-// <VideoButton />
 
 import Dialog from '../containers/Dialog';
 import Panel from '../containers/Panel';
-
-//          <ContentBlock type="Photo" data={logo} />
-//          <ContentBlock type="Video" data="https://youtu.be/9bZkp7q19f0" />
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Dialog />
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
         <Panel />
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
       </div>
     );
   }
