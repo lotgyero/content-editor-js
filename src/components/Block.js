4cd2924d8c9e3afccd8fd450e510707bc79aac07
Loty Gero <lotgyero@gmail.com>
2018-07-10 20:13:06 +0300

    A liked moved moved

--- src/components/Block.js
+++ src/components/Block.js
@@ -4,12 +4,28 @@ import PropTypes from 'prop-types';
 import Thumbnail from './Thumbnail';
 
 class ContentBlock extends React.Component {
+  state={
+    clientX: 0,
+    clientY: 0
+  }
   _handleOnDragEnd=(e)=>{
     console.log(this.props)
     const {clientX, clientY } = e;
     console.log({clientX, clientY });
-    this.props.blockMove(this.props.id, clientX, clientY);
+    const newClientX =  this.state.clientX + clientX;
+    const newClientY =  this.state.clientY + clientY ;
+    this.props.blockMove(this.props.id, newClientX, newClientY);
   }
+
+  _handleOnDragStart=(e)=>{
+    const {clientX, clientY } = e;
+    console.log('onDragStart', clientX, clientY )
+    this.setState({
+      clientX:  this.props.geometry.x - clientX,
+      clientY:  this.props.geometry.y - clientY
+    })
+  }
+
   _selectBlock=()=>{
     this.props.blockSelect(this.props.type, this.props.id);
   }
@@ -32,6 +48,7 @@ class ContentBlock extends React.Component {
              className="contentBlock"
              style={style}
              onClick={this._selectBlock}
+             onDragStart={this._handleOnDragStart}
              onDragEnd={this._handleOnDragEnd}
            >
       <Thumbnail data={this.props.data} type={this.props.type} geometry={this.props.geometry} />
